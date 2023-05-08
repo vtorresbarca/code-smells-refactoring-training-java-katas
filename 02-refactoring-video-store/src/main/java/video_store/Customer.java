25e06bd8fc55fd0f217f4734d83327db05f9ee4e
vtorres <vtorres@idealista.com>
2023-05-08 11:49:27 +0200

    Separamos el while en dos bucles para entenderlo mejor

--- 02-refactoring-video-store/src/main/java/video_store/Customer.java
+++ 02-refactoring-video-store/src/main/java/video_store/Customer.java
@@ -1,24 +1,20 @@
 package video_store;
 
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
-import java.util.Vector;
 
 public class Customer {
 
     private String name;
-    private Vector rentals = new Vector();
-
-    private List<Rental> rentalList = new ArrayList<>();
+    private List<Rental> rentals;
 
     public Customer(String name) {
         this.name = name;
+        rentals  = new ArrayList<>();
     }
 
     public void addRental(Rental rental) {
-        rentals.addElement(rental);
-        rentalList.add(rental);
+        rentals.add(rental);
     }
 
     public String getName() {
@@ -28,31 +24,31 @@ public class Customer {
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration rentals = this.rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
 
-        for (Rental rental: rentalList) {
-            double thisAmount = 0;
+/*
+        for (Rental rental: rentals) {
+            double thisAmount = rental.calculateAmount();
 
-            thisAmount += rental.calculateAmount();
             frequentRenterPoints += rental.calculateFrequentPoints();
 
             result += "\t" + rental.getTitle() + "\t"
                     + thisAmount + "\n";
             totalAmount += thisAmount;
         }
+*/
 
-/*        while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
-            Rental rental = (Rental) rentals.nextElement();
-
-            thisAmount += rental.calculateAmount();
+        for (Rental rental: rentals) {
             frequentRenterPoints += rental.calculateFrequentPoints();
+        }
 
+        for (Rental rental: rentals) {
+            double thisAmount = 0;
+            thisAmount += rental.calculateAmount();
             result += "\t" + rental.getTitle() + "\t"
-                + thisAmount + "\n";
+                    + thisAmount + "\n";
             totalAmount += thisAmount;
-        }*/
+        }
 
         result += "You owed " + totalAmount + "\n";
         result += "You earned " + frequentRenterPoints + " frequent renter points\n";

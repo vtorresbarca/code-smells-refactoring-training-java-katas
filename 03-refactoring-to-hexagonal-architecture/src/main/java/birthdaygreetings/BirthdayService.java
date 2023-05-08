92c0b1f77f238261b70bf914210e0dc6ddcbb4b1
vtorres <vtorres@idealista.com>
2023-05-08 13:42:48 +0200

    Split del while para dividir la responsabilidad de los empleados

--- 03-refactoring-to-hexagonal-architecture/src/main/java/birthdaygreetings/BirthdayService.java
+++ 03-refactoring-to-hexagonal-architecture/src/main/java/birthdaygreetings/BirthdayService.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -19,12 +21,17 @@ public class BirthdayService {
             String smtpHost, int smtpPort) throws IOException, ParseException,
             AddressException, MessagingException {
         BufferedReader in = new BufferedReader(new FileReader(fileName));
+        List<Employee> employees = new ArrayList<>();
         String str = "";
         str = in.readLine(); // skip header
         while ((str = in.readLine()) != null) {
             String[] employeeData = str.split(", ");
             Employee employee = new Employee(employeeData[1], employeeData[0],
                     employeeData[2], employeeData[3]);
+            employees.add(employee);
+        }
+
+        for(Employee employee: employees) {
             if (employee.isBirthday(ourDate)) {
                 String recipient = employee.getEmail();
                 String body = "Happy Birthday, dear %NAME%!".replace("%NAME%",

3a4e2ad294295131e07d9624668f17b29c1e7d34
vtorres <vtorres@idealista.com>
2023-05-08 11:26:09 +0200

    Creamos lista nueva para sustituir el vector

--- 02-refactoring-video-store/src/main/java/video_store/Customer.java
+++ 02-refactoring-video-store/src/main/java/video_store/Customer.java
@@ -1,6 +1,8 @@
 package video_store;
 
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Vector;
 
 public class Customer {
@@ -8,12 +10,15 @@ public class Customer {
     private String name;
     private Vector rentals = new Vector();
 
+    private List<Rental> rentalList = new ArrayList<>();
+
     public Customer(String name) {
         this.name = name;
     }
 
     public void addRental(Rental rental) {
         rentals.addElement(rental);
+        rentalList.add(rental);
     }
 
     public String getName() {
@@ -26,7 +31,18 @@ public class Customer {
         Enumeration rentals = this.rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
 
-        while (rentals.hasMoreElements()) {
+        for (Rental rental: rentalList) {
+            double thisAmount = 0;
+
+            thisAmount += rental.calculateAmount();
+            frequentRenterPoints += rental.calculateFrequentPoints();
+
+            result += "\t" + rental.getTitle() + "\t"
+                    + thisAmount + "\n";
+            totalAmount += thisAmount;
+        }
+
+/*        while (rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental rental = (Rental) rentals.nextElement();
 
@@ -36,7 +52,7 @@ public class Customer {
             result += "\t" + rental.getTitle() + "\t"
                 + thisAmount + "\n";
             totalAmount += thisAmount;
-        }
+        }*/
 
         result += "You owed " + totalAmount + "\n";
         result += "You earned " + frequentRenterPoints + " frequent renter points\n";
